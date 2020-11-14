d9c602f924083c2974358c37903c4de2830b9172
vla5924 <vla5924@gmail.com>
2020-11-15 00:06:52 +0300

    compiler::exec generic method

--- compiler/include/compiler.h
+++ compiler/include/compiler.h
@@ -2,5 +2,6 @@
 
 class Compiler
 {
-
+public:
+    static int exec(int argc, char* argv[]);
 };
--- compiler/src/compiler.cpp
+++ compiler/src/compiler.cpp
@@ -1,1 +1,6 @@
 #include "compiler.h"
+
+int Compiler::exec(int argc, char* argv[])
+{
+    return 0;
+}
