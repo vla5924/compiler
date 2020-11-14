93390f54535c0a650cd09a7d3bd205eda4aa61b6
vla5924 <vla5924@gmail.com>
2020-11-15 01:03:11 +0300

    common Token struct

---
+++ backend/include/token.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <string>
+
+struct Token
+{
+    enum class Keyword
+    {
+        Begin,
+        End,
+        Var,
+        Integer,
+        If,
+        Then,
+        Else,
+        While,
+        Do,
+        For,
+        To,
+        Repeat,
+        Until
+    };
+
+    enum class Operator
+    {
+        Colon,
+        Semicolon,
+        Stop,
+        Assign,
+        Plus,
+        Minus,
+        Mult,
+        Div,
+        Equal,
+        NotEqual,
+        Less,
+        More,
+        LessEqual,
+        MoreEqual,
+        LeftBrace,
+        RightBrace
+    };
+
+    enum class Type
+    {
+        Keyword,
+        Identifier,
+        Operator,
+        IntegerLiteral,
+        FloatingPointLiteral,
+        StringLiteral
+    };
+    Type type;
+
+    union Value
+    {
+        Keyword kwValue;
+        std::string idValue;
+        Operator opValue;
+        long intValue;
+        double fpValue;
+        std::string strValue;
+    };
+};
