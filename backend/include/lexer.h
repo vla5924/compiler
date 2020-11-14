531607e78a981f9b7af0fc705309ac5d56480196
vla5924 <vla5924@gmail.com>
2020-11-15 01:03:40 +0300

    lexer prototype with kw/ops maps

--- backend/include/lexer.h
+++ backend/include/lexer.h
@@ -1,8 +1,16 @@
 #pragma once
 
-#include "tokens/basetoken.h"
+#include <string_view>
+#include <list>
+#include <string>
+#include <map>
+
+#include "token.h"
 
 class Lexer
 {
-
+    static std::map<std::string_view, Token::Keyword> keywords;
+    static std::map<std::string_view, Token::Operator> operators;
+public:
+    static std::list<Token> process(std::string_view source);
 };
--- backend/src/lexer.cpp
+++ backend/src/lexer.cpp
@@ -1,1 +1,43 @@
 #include "lexer.h"
+
+std::map<std::string_view, Token::Keyword> Lexer::keywords = {
+    { "begin", Token::Keyword::Begin },
+    { "end", Token::Keyword::End },
+    { "var", Token::Keyword::Var },
+    { "integer", Token::Keyword::Integer },
+    { "if", Token::Keyword::If },
+    { "then", Token::Keyword::Then },
+    { "else", Token::Keyword::Else },
+    { "while", Token::Keyword::While },
+    { "do", Token::Keyword::Do },
+    { "for", Token::Keyword::For },
+    { "to", Token::Keyword::To },
+    { "repeat", Token::Keyword::Repeat },
+    { "until", Token::Keyword::Until },
+};
+
+std::map<std::string_view, Token::Operator> Lexer::operators = {
+    { ":", Token::Operator::Colon },
+    { ";", Token::Operator::Semicolon },
+    { ".", Token::Operator::Stop },
+    { ":=", Token::Operator::Assign },
+    { "+", Token::Operator::Plus },
+    { "-", Token::Operator::Minus },
+    { "*", Token::Operator::Mult },
+    { "/", Token::Operator::Div },
+    { "=", Token::Operator::Equal },
+    { "<>", Token::Operator::NotEqual },
+    { "<", Token::Operator::Less },
+    { ">", Token::Operator::More },
+    { "<=", Token::Operator::LessEqual },
+    { ">=", Token::Operator::MoreEqual },
+    { "(", Token::Operator::LeftBrace },
+    { ")", Token::Operator::RightBrace },
+};
+
+std::list<Token> Lexer::process(std::string_view source)
+{
+    std::list<Token> tokens;
+    // TODO
+    return tokens;
+}
