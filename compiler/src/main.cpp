edb23cc73330460746707f6c2aafd351654a7ca4
vla5924 <vla5924@gmail.com>
2020-11-21 20:51:20 +0300

    update str definition in main

--- compiler/src/main.cpp
+++ compiler/src/main.cpp
@@ -2,18 +2,21 @@
 #include <string>
 
 #include <preprocessor.h>
+#include <stringvec.h>
 
 #include "compiler.h"
 
 int main(int argc, char* argv[])
 {
     std::cout << "Compiler launched.\n";
-    std::string str = "// hello\n" 
-    "how 'are' you?\n"
-    "what is // your name?\n"
-    "how 'old' // are 'you'?\n"
-    "how do 'you //feel' today?\n"
-    "do you 'like' the '//weather' // today?";
+    StringVec str = {
+        "// hello", 
+        "how 'are' you?",
+        "what is // your name?",
+        "how 'old' // are 'you'?",
+        "how do 'you //feel' today?",
+        "do you 'like' the '//weather' // today?"
+    };
     std::cout << Preprocessor::process(str);
 
     return Compiler::exec(argc, argv);
