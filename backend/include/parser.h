e050c4991e11416efca7f84d9022f8392a7a3117
vla5924 <vla5924@gmail.com>
2020-11-21 21:48:41 +0300

    parseLiteral prototype

--- backend/include/parser.h
+++ backend/include/parser.h
@@ -1,7 +1,15 @@
 #pragma once
 
+#include <string>
+#include <string_view>
+#include <cstdlib>
+
+#include "token.h"
+
 class Parser
 {
+    template<typename RType>
+    static RType parseLiteral(const Token& token);
 public:
     Parser() = delete;
     Parser(const Parser&) = delete;
--- backend/src/parser.cpp
+++ backend/src/parser.cpp
@@ -1,1 +1,31 @@
 #include "parser.h"
+
+template<>
+int Parser::parseLiteral(const Token& token)
+{
+    return atoi(token.literal().c_str());
+}
+
+template<>
+long Parser::parseLiteral(const Token& token)
+{
+    return atol(token.literal().c_str());
+}
+
+template<>
+double Parser::parseLiteral(const Token& token)
+{
+    return atof(token.literal().c_str());
+}
+
+template<>
+std::string_view Parser::parseLiteral(const Token& token)
+{
+    return token.literal();
+}
+
+template<>
+std::string Parser::parseLiteral(const Token& token)
+{
+    return token.literal();
+}
